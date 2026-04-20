50b49be5a759523b59737a0f92ffdbee140f8d04
AlexisWUS <alexis.warman@elevietravel.com>
2026-04-19 20:50:54 -0600

    Update Supabase URL in supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ibdvvntcahiodeujkozb.supabase.co';
+const supabaseUrl = 'https://ibdvwntcahiodeujkozb.supabase.co';
 const supabaseKey = 'sb_publishable_2GC65x_HBOaNT5sBDDxQVQ_RekXbw6G';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
